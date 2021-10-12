5ed56b64e45036f5cb3e50b4b158584d79d1e54b
Percy Chen <abc2344666@gmail.com>
2021-10-12 16:38:39 +0800

    fix: insertion sort response

--- insertion_sort.py
+++ insertion_sort.py
@@ -10,10 +10,9 @@ def insertion_sort(lst: list) -> list:
             lst[idx_2 + 1] = lst[idx_2]
             idx_2 -= 1
         lst[idx_2 + 1] = cur_val
-    print(lst)
+    return lst
 
 
 list_a = random.sample([i for i in range(100)], 7)
 print(f"Before insertion sort: {list_a}")
-insertion_sort(list_a)
-print(f"After insertion sort: {list_a}")
+print(f"After insertion sort: {insertion_sort(list_a)}")
