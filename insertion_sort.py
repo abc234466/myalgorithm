396c57f2aa7c8b96c0ca584aa6fa681a49cec28e
Percy Chen <abc2344666@gmail.com>
2021-10-12 14:37:46 +0800

    fix: insertion sort ver.2

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,14 +4,12 @@ import random
 def insertion_sort(lst: list) -> list:
     # import ipdb; ipdb.set_trace()
     for idx_1 in range(1, len(lst)):
+        cur_val = lst[idx_1]
         idx_2 = idx_1 - 1
-        while idx_1 > 0:
-            if lst[idx_1] > lst[idx_2]:
-                break
-            else:
-                lst[idx_1], lst[idx_2] = lst[idx_2], lst[idx_1]
-                idx_2 -= 1
-            idx_1 -= 1
+        while cur_val < lst[idx_2] and idx_2 >= 0:
+            lst[idx_2 + 1] = lst[idx_2]
+            idx_2 -= 1
+        lst[idx_2 + 1] = cur_val
     print(lst)
 
 
