c3281e0aed9f42332bdd9a0f36b963a478319504
Percy Chen <abc2344666@gmail.com>
2021-10-14 11:36:41 +0800

    feat: dp fibonacci

--- fib.py
+++ fib.py
@@ -1,3 +1,6 @@
+count = 40
+
+
 def fibonacci(n):
     if n == 0:
         return 0
@@ -7,7 +10,7 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 
-print(fibonacci(15))
+print(fibonacci(count))
 
 
 def tail_fibonacci(n, init, accumulator):
@@ -17,4 +20,19 @@ def tail_fibonacci(n, init, accumulator):
         return tail_fibonacci(n-1, accumulator, init + accumulator)
 
 
-print(tail_fibonacci(16, 0, 1))
+print(tail_fibonacci(count, 0, 1))
+
+lookup = {}
+
+def dp_fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        if n in lookup:
+            return lookup[n]
+        else:
+            lookup[n] = dp_fibonacci(n-1) + dp_fibonacci(n-2)
+            return lookup[n]
+
+
+print(dp_fibonacci(count))
